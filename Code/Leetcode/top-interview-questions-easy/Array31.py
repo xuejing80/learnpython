702202ef2aa560d4466e26cfae66082a0319d564
xuejing80 <xuejing_cn@163.com>
2019-07-14 08:16:18 +0800

    更新：旋转图像的方案二

--- Code/Leetcode/top-interview-questions-easy/Array31.py
+++ Code/Leetcode/top-interview-questions-easy/Array31.py
@@ -6,8 +6,8 @@
 最后修改于：2019/07/14
 '''
 
-# 这个题目的难度，不在编程，而在数学建模的环节，也就是说，我们为了找到旋转列表元
-# 素的规律，必须在白纸上把原来的坐标和新坐标写下来，并找出其中的对应关系
+# 方案一，这个题目的难度，不在编程，而在数学建模的环节，也就是说，我们为了找到旋
+# 转列表元素的规律，必须在白纸上把原来的坐标和新坐标写下来，并找出其中的对应关系
 # 该方案战胜 98.97 % 的 python3 提交记录
 class Solution:
     def rotate(self, matrix: list) -> None:
@@ -23,6 +23,20 @@ class Solution:
         # 遍历字典，按照新坐标，将数据挨个放回去
         for pos,value in newPosition.items():
             matrix[pos[0]][pos[1]] = value
+
+# 方案二，题目规定不允许借助其他的数据结构，这就比较麻烦了，我把每一次旋转归纳成
+# 4个步骤，然后只需要遍历原二维列表中的1/4的元素即可完成整个旋转操作，真心累！
+# 该方案战胜 96.39 % 的 python3 提交记录
+class Solution:
+    def rotate(self, matrix: list) -> None:
+        row, col = len(matrix), len(matrix[0])
+        for i in range((row+1)//2):
+            for j in range(col//2):
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[row-1-j][i]
+                matrix[row-1-j][i] = matrix[row-1-i][col-1-j]
+                matrix[row-1-i][col-1-j] = matrix[j][col-1-i] 
+                matrix[j][col-1-i]  = tmp
 
 # 以下是本地测试代码，提交时只需复制上面的代码块即可
 solution = Solution()
